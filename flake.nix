1753c67893df550d0c07e635732a89f8eb14eb4f
Enigmage <alizaidi0786.az@gmail.com>
2023-07-17 15:20:18 +0530

    nix reproducible dev environment

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Python dev environment for InstaAudio";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "InstaAudio server dev shell";
+        buildInputs = with pkgs; [
+          python311
+          python311Packages.pip
+        ];
+        LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+      };
+    };
+}
+
